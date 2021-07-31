5ba09c00c1f1752bde5416e68fe433e19dbc2daf
Ubuntu <ubuntu@ip-172-31-43-239.ap-southeast-1.compute.internal>
2021-07-31 01:50:01 +0000

    env var environ

--- myproject/settings.py
+++ myproject/settings.py
@@ -28,13 +28,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ['SECRET_KEY']
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool('DJANGO_DEBUG', False)
 
-allowed_hosts = os.environ['ALLOWED_HOSTS']
-ALLOWED_HOSTS = allowed_hosts.split(',')
+ALLOWED_HOSTS = env.list('ALLOWED_HOSTS')
 
 # Application definition
 
@@ -113,7 +112,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'id'
 
-TIME_ZONE = 'Asia/Jakarta'
+TIME_ZONE = 'UTC'
 
 USE_I18N = True
 
