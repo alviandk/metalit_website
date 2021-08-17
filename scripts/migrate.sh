1628e5a7a29f80261db4bc22c6d623e567d63ad2
alvian.kurnianto <alvian.kurnianto@mekar.id>
2021-08-17 13:46:41 +0700

    add pip install script

--- scripts/migrate.sh
+++ scripts/migrate.sh
@@ -1,4 +1,6 @@
 cd /home/ubuntu/myprojectdir
 source /home/ubuntu/myprojectdir/myprojectenv/bin/activate
 export $(grep -v '^#' .env | xargs)
+pip install --upgrade pip
+pip install -r requirements.txt
 ./manage.py migrate
