b541f19e95f943c8fcd6946101e7a68a823c0f04
Vika Putri Ariyanti <vikaputriariyanti@gmail.com>
2021-08-19 23:32:22 +0700

    about us

--- revamp/views.py
+++ revamp/views.py
@@ -3,3 +3,5 @@ from django.shortcuts import render, redirect
 # Create your views here.
 def index(request):
 	return render(request, 'revamp/index.html')
+def about(request):
+	return render(request, 'revamp/about.html')
