d1095f436d852172ee8362eb55505de0c3aafb9a
Vika Putri Ariyanti <vikaputriariyanti@gmail.com>
2021-08-24 11:21:12 +0700

    form contact

--- revamp/models.py
+++ revamp/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Contact(models.Model):
+	nama = models.CharField(max_length=255)
+	email = models.EmailField()
+	pesan = models.TextField()
+
+	def __str__(self):
+		return self.email
