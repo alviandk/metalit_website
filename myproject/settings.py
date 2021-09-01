bac94ad25a716d35a20f3da7f3f9cd154e0f6355
Vika Putri Ariyanti <vikaputriariyanti@gmail.com>
2021-09-01 20:08:53 +0700

    footer

--- myproject/settings.py
+++ myproject/settings.py
@@ -184,7 +184,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 MESSAGE_TAGS = {
         messages.DEBUG: 'alert-secondary',
         messages.INFO: 'alert-info',
-        messages.SUCCESS: 'alert-success',
         messages.WARNING: 'alert-warning',
         messages.ERROR: 'alert-danger',
+        messages.SUCCESS: 'alert-success',
 }
