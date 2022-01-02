6ff869c5cbaf4267f4d21828a1db8d014e6695e2
alviandk <alviandk@GMAIL.COM>
2022-01-02 20:35:17 +0700

    Update migrate.sh

--- scripts/migrate.sh
+++ scripts/migrate.sh
@@ -2,5 +2,3 @@ cd /home/ubuntu/metalit_website
 source /home/ubuntu/virtmetalit/bin/activate
 export $(grep -v '^#' .env | xargs)
 pip install --upgrade pip
-pip install -r requirements.txt
-./manage.py migrate
