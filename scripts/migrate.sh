11e6c38c1a93a0c9f899d9a84e14c49be1b69dee
alviandk <alviandk@GMAIL.COM>
2022-01-02 20:28:14 +0700

    Update migrate.sh

--- scripts/migrate.sh
+++ scripts/migrate.sh
@@ -2,5 +2,3 @@ cd /home/ubuntu/myprojectdir
 source /home/ubuntu/myprojectdir/myprojectenv/bin/activate
 export $(grep -v '^#' .env | xargs)
 pip install --upgrade pip
-pip install -r requirements.txt
-./manage.py migrate
